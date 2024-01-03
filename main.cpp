43b961e631446051b0e6ad5b990ae2c8abc713ac
Bogoslovsky.Fed <Fedor.Bogoslovsky@infotecs.ru>
2024-01-03 15:24:34 +0300

    Update switch/case logic

--- main.cpp
+++ main.cpp
@@ -42,65 +42,95 @@ int main(int argc, char* argv[], char* envp[]) {
                         std::cout << "File: " << fileName << std::endl;
                         std::cout << "Mutation...  " << fileName << std::endl;
                         std::string arg = "  "; 
-                        for(int i = 1; i<=27; i++)
+                        int i =1;
+                        for(i = 1; i<=27; i++)
                         {
                             switch(i) {
                                 case 1 : 
                                     arg = " -m fn=2"; 
+                                    break;
                                 case 2 : 
                                     arg = " -m fo=2"; 
+                                    break;
                                 case 3 : 
                                     arg = " -m fn";
+                                    break;
                                 case 4 : 
                                     arg = " -m num=5 ";
+                                    break;
                                 case 5 : 
                                     arg = " -m ld ";
+                                    break;
                                 case 6 : 
                                     arg = " -m lds";
+                                    break;
                                 case 7 :
                                     arg = " -m lr2";
+                                    break;
                                 case 8 :
                                     arg = " -m li";
+                                    break;
                                 case 9 : 
                                     arg = " -m ls";
+                                    break;
                                 case 10 : 
                                     arg = " -m lp";
+                                    break;
                                 case 11 : 
                                     arg = " -m lr";
+                                    break;
                                 case 12 : 
                                     arg = " -m lis"; 
+                                    break;
                                 case 13 : 
                                     arg = " -m lrs"; 
+                                    break;
                                 case 14 : 
                                     arg = " -m sr";
+                                    break;
                                 case 15 : 
                                     arg = " -m sd";
+                                    break;
                                 case 16 : 
                                     arg = " -m bd";
+                                    break;
                                 case 17 : 
                                     arg = " -m bf";
+                                    break;
                                 case 18 : 
                                     arg = " -m bi ";
+                                    break;
                                 case 19 : 
                                     arg = " -m br"; 
+                                    break;
                                 case 20 : 
                                     arg = " -m bp";
+                                    break;
                                 case 21 : 
                                     arg = " -m bei";
+                                    break;
                                 case 22 : 
                                     arg = " -m bed";
+                                    break;
                                 case 23 : 
                                     arg = " -m ber";
+                                    break;
                                 case 24 : 
                                     arg = " -m uw";
+                                    break;
                                 case 25 : 
                                     arg = " -m ui=2";
+                                    break;
                                 case 26 : 
                                     arg = " -m xp=9"; 
                                 case 27 : 
                                     arg = " -m ab"; 
-                                    }
-                                
+                                    break;
+                                default :
+                                    std::cout<<"Opps"<<i<<std::endl;
+                                    return -1;          
+                                }
+
                         mutation(fileName, arg , i);
                         }
     
